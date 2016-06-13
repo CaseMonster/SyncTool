898c02eee54fe2ca8a025cc2b562850976bbcdd5
Casey Schneberger <cschneberger@gmail.com>
2016-06-13 18:57:31 -0500

    Initial push of Run Class

---
+++ SyncTool/Run.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace SyncTool
+{
+    class Run
+    {
+        public void Execute(string dir, string file, string args)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = file;
+            p.StartInfo.WorkingDirectory = dir;
+            p.StartInfo.Arguments = args;
+
+            if (file == "null" || file == null)
+            {
+                Log.Info("executable not found");
+            }
+            else
+            {
+                try
+                {
+                    Log.Info("running executable");
+                    p.Start();
+                }
+                catch (System.ComponentModel.Win32Exception ex)
+                {
+                    Log.Error("could not run program");
+                    Log.Info(ex.ToString());
+                    return;
+                };
+            };
+        }
+    }
+}
