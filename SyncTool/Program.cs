3a263bca0ebbc28c164b57b399a02a8e6fd58956
Casey Schneberger <cschneberger@gmail.com>
2016-06-12 20:42:38 -0500

    Game Plan

--- SyncTool/Program.cs
+++ SyncTool/Program.cs
@@ -10,6 +10,27 @@ namespace SyncTool
     {
         static void Main(string[] args)
         {
+            //load settings (settings.xml)
+
+            //load local repo info (repo.xml), generate if doesn't exist
+
+            //generate object chain of loaded dirs/pbos
+
+            //download remote repo (http://rollingkeg.com/repo/repo.xml) -> (remote.xml)
+
+            //generate object chain of loaded dirs/pbos
+
+            //create list of pbos that have changed, hashes that have changed
+
+            //cycle list of pbo downloads, store in temp location
+
+            //hash downloaded pbos, compare to remote list of pbos
+
+            //replace local pbos from temp location
+
+            //update local xml as replace
+
+            //compare two xml checksums for pbos, again
         }
     }
 }
