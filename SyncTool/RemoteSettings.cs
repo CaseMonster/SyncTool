c95df808a070aa1c1de3177ca52bc6251e82f4ab
Casey Schneberger <cschneberger@gmail.com>
2016-06-13 19:54:06 -0500

    Remote Settings, parse mods

--- SyncTool/RemoteSettings.cs
+++ SyncTool/RemoteSettings.cs
@@ -4,10 +4,17 @@ namespace SyncTool
     class RemoteSettings
     {
         public string mods = "NULL";
+        public string []modsArray;
 
-        public RemoteSettings(string mods)
+        public RemoteSettings(string s)
         {
-            this.mods = mods;
+            this.mods = s;
+            ParseRemoteSettings(s);
+        }
+
+        public void ParseRemoteSettings(string s)
+        {
+            this.modsArray = s.Split(';');
         }
     }
 }
