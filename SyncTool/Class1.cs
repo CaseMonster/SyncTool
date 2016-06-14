8f2b96ec0d7a24f9f917b70df305416ee6553476
Tyler Leverre-Smith <tleverresmith@gmail.com>
2016-06-13 19:42:28 -0600

    :up: Attempt 2

---
+++ SyncTool/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SyncTool
+{
+    class FileHandler
+    {
+        public static void GenerateLocalRepo(string basePath)
+        {
+            // Recurse through the directory
+
+            string[] files = Directory.GetFiles(basePath, "*.*", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string filePath = file.TrimEnd(fileName.ToCharArray());
+                // for each file get the hash
+                string fileHash = HashGenerator.GetHash(file);
+
+                // store data in the XML file
+                Console.WriteLine(file);
+                XML.OutputToXML(fileName, filePath, fileHash, basePath);
+            }
+        }
+    }
+}
---
+++ SyncTool/SyncTool/Log.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace SyncTool
+{
+    class Log
+    {
+        public static string LOGFILE = "synctool.Log";
+        static StreamWriter log;
+
+        public static void Startup()
+        {
+            Info("SyncTool Started");
+        }
+
+        public static void Info(string s)
+        {
+            Write(LinePrep(s));
+        }
+
+        public static void Error(string s)
+        {
+            Write(LinePrep(s));
+        }
+
+        static string LinePrep(string s)
+        {
+            return (Environment.NewLine + "[" + DateTime.Now.ToString("yyyyMMMdd HHmm") + "]  "
+                + Environment.NewLine + "   " + s
+                );
+        }
+
+        static void Write(string s)
+        {
+            log = new StreamWriter(LOGFILE, true);
+            log.WriteLine(s);
+            log.Close();
+        }
+    }
+}
